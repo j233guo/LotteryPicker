810cead65e4b2fd3f361438af0868990cb4a8c9f
Jiaming Guo <j23guo@gmail.com>
2022-08-19 12:29:04 -0400

    Initial Commit

---
+++ LotteryPicker/LotteryPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LotteryPickerApp.swift
+//  LotteryPicker
+//
+//  Created by Jiaming Guo on 2022-08-19.
+//
+
+import SwiftUI
+
+@main
+struct LotteryPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
